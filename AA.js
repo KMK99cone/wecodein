2a050a44f20ecb496d9733ea2b0a0be6f39e8124
KMK99cone <105492581+KMK99cone@users.noreply.github.com>
2022-09-07 13:31:14 +0900

    Add files via upload

---
+++ AA.js
@@ -0,0 +1,8 @@
+<script>
+    let current = new Date();
+    if (current.time > 10) {
+        document.body.style.backgroundColor = "#FF8585";
+    }else{
+        document.body.style.backgroundColor =  "#FFDAB9";}
+
+</script>
